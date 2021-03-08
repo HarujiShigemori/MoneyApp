9034ae2f7e481edd16eefd2bdef75724abe203c3
Haruji Shigemori <test0302sh@gmail.com>
2021-03-09 05:14:09 +0900

    計算ページ１の結果の表示ができました

--- MoneyApp/Controller/ShowCalcController/Result1ViewController.swift
+++ MoneyApp/Controller/ShowCalcController/Result1ViewController.swift
@@ -16,19 +16,41 @@ class Result1ViewController: UIViewController {
     
     var calculatorModel = CalculatorModel()
     
+    @IBOutlet weak var startLabel: UILabel!
+    @IBOutlet weak var monthlyLabel: UILabel!
+    @IBOutlet weak var yearsLabel: UILabel!
+    @IBOutlet weak var annualYieldLabel: UILabel!
+    
+    @IBOutlet weak var InvestmentAmountLabel: UILabel!
+    @IBOutlet weak var revenueLabel: UILabel!
+    @IBOutlet weak var lastResultLabel: UILabel!
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let array = calculatorModel.calcFinal(start: start!, monthly: monthly!, years: years!, annualYield: annualYield!)
-        print(array)
-        
+        showResult()
         // Do any additional setup after loading the view.
     }
     
-
+    private func showResult() {
+        
+        let array = calculatorModel.calcFinal(start: start!, monthly: monthly!, years: years!, annualYield: annualYield!)
+        startLabel.text = "\(String(start!))円"
+        monthlyLabel.text = "\(String(monthly!))円"
+        yearsLabel.text = "\(String(years!))年"
+        annualYieldLabel.text = "\(String(annualYield!))%"
+        
+        InvestmentAmountLabel.text = "\(String(array.investmentAmount))円"
+        revenueLabel.text = "\(String(array.interest))円"
+        lastResultLabel.text = "\(String(array.result.last!))円"
+    }
+    
+    @IBAction func backButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
--- MoneyApp/Model/CalculatorModel.swift
+++ MoneyApp/Model/CalculatorModel.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class CalculatorModel {
     
-    func calcFinal(start:Int,monthly:Int,years:Int,annualYield:Int) -> Array<Int>{
+    func calcFinal(start:Int,monthly:Int,years:Int,annualYield:Int) -> (result:Array<Int>, investmentAmount:Int , interest:Int){
 
         let totalMonth = years * 12
 
@@ -17,26 +17,29 @@ class CalculatorModel {
         let month:Double = 1 / 12
         let monthlyInterest = pow(Double(convert), month)
 
-        var resultMoney:Double = Double(start)
+        var resultMoney:Int = start
         var result = [Int]()
 
         for x in 0..<totalMonth {
             let month = x + 1
             
-            let total = Double(monthly) * pow(monthlyInterest, Double(month))
+            let total = Int(Double(monthly) * pow(monthlyInterest, Double(month)))
             resultMoney += total
             
             if month % 12 == 0 {
                 
-                let startRes = Double(start) * pow(convert, Double(month / 12)) - Double(start) * pow(convert, Double(month / 12 - 1))
+                let startRes = Int(Double(start) * pow(convert, Double(month / 12)) - Double(start) * pow(convert, Double(month / 12 - 1)))
                 
                 resultMoney += startRes
                 
-                result.append(Int(resultMoney))
+                result.append(resultMoney)
                 
             }
         }
         
-        return result
+        let investmentAmount = start + monthly * totalMonth
+        let interest = result.last! - investmentAmount
+        
+        return (result,investmentAmount,interest)
     }
 }
